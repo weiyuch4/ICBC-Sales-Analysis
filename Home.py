95ebae83174eba68105c68ea035064f97a5f1790
weiyuch4 <yuche.wei@mail.utoronto.ca>
2023-03-13 20:48:37 -0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("# ICBC Sales Analysis :bookmark_tabs:")
+st.subheader("Sussex Insurance Agency (160 St)")
+
+st.markdown(
+    """
+    This program allows business owners and managers to compare their sales data and transaction breakdown over time 
+    and track their progress towards revenue goals. With this program, users can: 
+    - Compare sales data for any two specific dates, and view a detailed breakdown of transactions based on 
+    their types, such as renewal, temporary permit, or special coverages
+    - Organize business transactions into categories, compute the total amount collected for each category on a 
+    monthly basis, and view the data as a histogram for better visualization
+    - Discover the top ten postal codes with the largest customer base
+    - Categorize clients into different age groups and calculate the total number of clients in each group
+    - Retrieve and review the total sales figures up to the present day
+    """
+)
