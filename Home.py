fd1332ca550c59dcd7083c67c1bc50b893270e10
weiyuch4 <yuche.wei@mail.utoronto.ca>
2023-03-24 17:42:26 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,8 +11,11 @@ st.markdown(
     their types, such as renewal, temporary permit, or special coverages
     - Organize business transactions into categories, compute the total amount collected for each category on a 
     monthly basis, and view the data as a histogram for better visualization
+    - Enables pattern recognition and generates a histogram for enhanced visualization by classifying financial 
+    transactions based on their occurrence time and day of the week
     - Discover the top ten postal codes with the largest customer base
     - Categorize clients into different age groups and calculate the total number of clients in each group
-    - Retrieve and review the total sales figures up to the present day
+    - Collect and examine the cumulative sales data up to the current date, and compare it with the sales data from 
+    the same date in the previous year for the specified year
     """
 )
